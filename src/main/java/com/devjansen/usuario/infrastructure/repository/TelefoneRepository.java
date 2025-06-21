8e75ae5074c211200e365364c60434bf7a716c79
Jhonata Jansen <codejhonatajansen@gmail.com>
2025-06-21 00:46:44 -0300

    adicionadas classes de security

---
+++ src/main/java/com/devjansen/usuario/infrastructure/repository/TelefoneRepository.java
@@ -0,0 +1,9 @@
+package com.devjansen.usuario.infrastructure.repository;
+
+import com.devjansen.infrastructure.entity.Telefone;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TelefoneRepository extends JpaRepository<Telefone, Long> {
+}
