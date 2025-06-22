b2065a2a1f8b2bb9884de042454d2b6c27345f2a
Jhonata Jansen <codejhonatajansen@gmail.com>
2025-06-22 16:14:34 -0300

    adicionado metodos get, delete e login

---
+++ src/main/java/com/devjansen/usuario/infrastructure/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,10 @@
+package com.devjansen.usuario.infrastructure.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+    public ResourceNotFoundException(String message, Throwable throwable){
+        super(message, throwable);
+    }
+}
